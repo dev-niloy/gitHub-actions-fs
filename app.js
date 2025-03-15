a3f2485e26dc75778d3c215b820f333d936ce9c0
dev-niloy <niloyroy184@gmail.com>
2025-03-15 13:14:28 +0600

    addded custom workflows with custom yaml

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(201).json({ message: "Hello World from server api response!" });
+  res.send("Hello World from server api response!");
 });
 
 module.exports = app;
